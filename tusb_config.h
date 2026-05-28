45d661c39a3334f0bf7dced7d9e084dcf7813c27
heyjoeway <joe@jojudge.com>
2026-05-27 20:43:00 -0400

    USB HID prep

---
+++ tusb_config.h
@@ -0,0 +1,50 @@
+#ifndef _TUSB_CONFIG_H_
+#define _TUSB_CONFIG_H_
+
+#include "hardware/structs/usb.h"
+
+#ifndef CFG_TUSB_MCU
+#define CFG_TUSB_MCU OPT_MCU_RP2040
+#endif
+
+#ifndef CFG_TUSB_OS
+#define CFG_TUSB_OS OPT_OS_PICO
+#endif
+
+#ifndef CFG_TUSB_RHPORT0_MODE
+#define CFG_TUSB_RHPORT0_MODE OPT_MODE_DEVICE
+#endif
+
+#ifndef CFG_TUD_ENABLED
+#define CFG_TUD_ENABLED 1
+#endif
+
+#ifndef CFG_TUD_ENDPOINT0_SIZE
+#define CFG_TUD_ENDPOINT0_SIZE 64
+#endif
+
+#ifndef CFG_TUD_CDC
+#define CFG_TUD_CDC 1
+#endif
+
+#ifndef CFG_TUD_HID
+#define CFG_TUD_HID 1
+#endif
+
+#ifndef CFG_TUD_CDC_RX_BUFSIZE
+#define CFG_TUD_CDC_RX_BUFSIZE 256
+#endif
+
+#ifndef CFG_TUD_CDC_TX_BUFSIZE
+#define CFG_TUD_CDC_TX_BUFSIZE 256
+#endif
+
+#ifndef CFG_TUD_CDC_EP_BUFSIZE
+#define CFG_TUD_CDC_EP_BUFSIZE 64
+#endif
+
+#ifndef CFG_TUD_HID_EP_BUFSIZE
+#define CFG_TUD_HID_EP_BUFSIZE 16
+#endif
+
+#endif
---
+++ usb_descriptors.c
@@ -0,0 +1,186 @@
+#include "pico/stdio_usb.h"
+#include "pico/stdio_usb/reset_interface.h"
+#include "pico/unique_id.h"
+#include "tusb.h"
+
+#ifndef USBD_VID
+#define USBD_VID 0x2E8A
+#endif
+
+#ifndef USBD_PID
+#define USBD_PID 0x0009
+#endif
+
+#ifndef USBD_MANUFACTURER
+#define USBD_MANUFACTURER "Raspberry Pi"
+#endif
+
+#ifndef USBD_PRODUCT
+#define USBD_PRODUCT "Pico Wii HID Bridge"
+#endif
+
+#define TUD_RPI_RESET_DESC_LEN 9
+#define USBD_DESC_LEN (TUD_CONFIG_DESC_LEN + TUD_CDC_DESC_LEN + TUD_RPI_RESET_DESC_LEN + TUD_HID_DESC_LEN)
+
+#if !PICO_STDIO_USB_DEVICE_SELF_POWERED
+#define USBD_CONFIGURATION_DESCRIPTOR_ATTRIBUTE 0
+#define USBD_MAX_POWER_MA 250
+#else
+#define USBD_CONFIGURATION_DESCRIPTOR_ATTRIBUTE TUSB_DESC_CONFIG_ATT_SELF_POWERED
+#define USBD_MAX_POWER_MA 1
+#endif
+
+#define USBD_ITF_CDC 0
+#define USBD_ITF_RPI_RESET 2
+#define USBD_ITF_HID 3
+#define USBD_ITF_MAX 4
+
+#define USBD_CDC_EP_CMD 0x81
+#define USBD_CDC_EP_OUT 0x02
+#define USBD_CDC_EP_IN 0x82
+#define USBD_HID_EP_IN 0x83
+
+#define USBD_CDC_CMD_MAX_SIZE 8
+#define USBD_CDC_IN_OUT_MAX_SIZE 64
+#define USBD_HID_EP_SIZE 16
+#define USBD_HID_POLL_MS 1
+
+#define USBD_STR_0 0x00
+#define USBD_STR_MANUF 0x01
+#define USBD_STR_PRODUCT 0x02
+#define USBD_STR_SERIAL 0x03
+#define USBD_STR_CDC 0x04
+#define USBD_STR_RPI_RESET 0x05
+#define USBD_STR_HID 0x06
+
+#define TUD_RPI_RESET_DESCRIPTOR(_itfnum, _stridx) \
+    9, TUSB_DESC_INTERFACE, _itfnum, 0, 0, TUSB_CLASS_VENDOR_SPECIFIC, RESET_INTERFACE_SUBCLASS, RESET_INTERFACE_PROTOCOL, _stridx
+
+static const tusb_desc_device_t usbd_desc_device = {
+    .bLength = sizeof(tusb_desc_device_t),
+    .bDescriptorType = TUSB_DESC_DEVICE,
+#if PICO_STDIO_USB_ENABLE_RESET_VIA_VENDOR_INTERFACE && PICO_STDIO_USB_RESET_INTERFACE_SUPPORT_MS_OS_20_DESCRIPTOR
+    .bcdUSB = 0x0210,
+#else
+    .bcdUSB = 0x0200,
+#endif
+    .bDeviceClass = TUSB_CLASS_MISC,
+    .bDeviceSubClass = MISC_SUBCLASS_COMMON,
+    .bDeviceProtocol = MISC_PROTOCOL_IAD,
+    .bMaxPacketSize0 = CFG_TUD_ENDPOINT0_SIZE,
+    .idVendor = USBD_VID,
+    .idProduct = USBD_PID,
+    .bcdDevice = 0x0100,
+    .iManufacturer = USBD_STR_MANUF,
+    .iProduct = USBD_STR_PRODUCT,
+    .iSerialNumber = USBD_STR_SERIAL,
+    .bNumConfigurations = 1,
+};
+
+static const uint8_t hid_report_desc[] = {
+    TUD_HID_REPORT_DESC_MOUSE(HID_REPORT_ID(1)),
+};
+
+static const uint8_t usbd_desc_cfg[USBD_DESC_LEN] = {
+    TUD_CONFIG_DESCRIPTOR(
+        1,
+        USBD_ITF_MAX,
+        USBD_STR_0,
+        USBD_DESC_LEN,
+        USBD_CONFIGURATION_DESCRIPTOR_ATTRIBUTE,
+        USBD_MAX_POWER_MA),
+
+    TUD_CDC_DESCRIPTOR(
+        USBD_ITF_CDC,
+        USBD_STR_CDC,
+        USBD_CDC_EP_CMD,
+        USBD_CDC_CMD_MAX_SIZE,
+        USBD_CDC_EP_OUT,
+        USBD_CDC_EP_IN,
+        USBD_CDC_IN_OUT_MAX_SIZE),
+
+    TUD_RPI_RESET_DESCRIPTOR(USBD_ITF_RPI_RESET, USBD_STR_RPI_RESET),
+
+    TUD_HID_DESCRIPTOR(
+        USBD_ITF_HID,
+        USBD_STR_HID,
+        HID_ITF_PROTOCOL_NONE,
+        sizeof(hid_report_desc),
+        USBD_HID_EP_IN,
+        USBD_HID_EP_SIZE,
+        USBD_HID_POLL_MS),
+};
+
+static char usbd_serial_str[PICO_UNIQUE_BOARD_ID_SIZE_BYTES * 2 + 1];
+
+static const char *const usbd_desc_str[] = {
+    [USBD_STR_MANUF] = USBD_MANUFACTURER,
+    [USBD_STR_PRODUCT] = USBD_PRODUCT,
+    [USBD_STR_SERIAL] = usbd_serial_str,
+    [USBD_STR_CDC] = "Board CDC",
+    [USBD_STR_RPI_RESET] = "Reset",
+    [USBD_STR_HID] = "HID",
+};
+
+const uint8_t *tud_descriptor_device_cb(void) {
+    return (const uint8_t *)&usbd_desc_device;
+}
+
+const uint8_t *tud_descriptor_configuration_cb(uint8_t index) {
+    (void)index;
+    return usbd_desc_cfg;
+}
+
+const uint16_t *tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
+    (void)langid;
+
+#ifndef USBD_DESC_STR_MAX
+#define USBD_DESC_STR_MAX 20
+#endif
+    static uint16_t desc_str[USBD_DESC_STR_MAX];
+
+    if (!usbd_serial_str[0]) {
+        pico_get_unique_board_id_string(usbd_serial_str, sizeof(usbd_serial_str));
+    }
+
+    uint8_t len;
+    if (index == 0) {
+        desc_str[1] = 0x0409;
+        len = 1;
+    } else {
+        if (index >= sizeof(usbd_desc_str) / sizeof(usbd_desc_str[0])) {
+            return NULL;
+        }
+        const char *str = usbd_desc_str[index];
+        for (len = 0; len < USBD_DESC_STR_MAX - 1 && str[len]; ++len) {
+            desc_str[1 + len] = str[len];
+        }
+    }
+
+    desc_str[0] = (uint16_t)((TUSB_DESC_STRING << 8) | (2 * len + 2));
+    return desc_str;
+}
+
+const uint8_t *tud_hid_descriptor_report_cb(uint8_t instance) {
+    (void)instance;
+    return hid_report_desc;
+}
+
+uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type,
+                               uint8_t *buffer, uint16_t reqlen) {
+    (void)instance;
+    (void)report_id;
+    (void)report_type;
+    (void)buffer;
+    (void)reqlen;
+    return 0;
+}
+
+void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type,
+                           const uint8_t *buffer, uint16_t bufsize) {
+    (void)instance;
+    (void)report_id;
+    (void)report_type;
+    (void)buffer;
+    (void)bufsize;
+}
